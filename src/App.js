8d500e3b1452cca113401ae42e29878c00feaf9a
Sedna <60520922+MoonofSedna@users.noreply.github.com>
2020-02-19 21:53:22 -0500

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Form from './components/Form';
+import ImageList from './components/ImageList';
+import Error from './components/Error';
 
 function App() {
+
+
+  // State App
+
+  const [search, saveSearch]= useState('');
+  const [images, saveImages] = useState([]);
+  const [page, savePage] = useState(1);
+  const [totalpage, saveTotalPage] = useState(5);
+  const [error, saveError] = useState(false);
+  
+
+  useEffect ( () => {
+
+    const ConsultAPI = async () => {
+
+      if(search === '') return;
+
+
+    const ImagesbyPage = 30;
+    const key = '15309538-d75fce94a0987dd37a816cc1b';
+    const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${ImagesbyPage}&page=${page}`;
+    const reply = await fetch(url);
+    const result = await reply.json();
+
+    saveImages(result.hits);
+
+    // Validate results
+
+    if (result.hits.length === 0){
+      saveError(true);
+      return;
+      
+    }
+
+    saveError(false);
+
+    //Total Pages
+
+    const calculateTotalPages = Math.ceil(result.totalHits / ImagesbyPage);
+
+    saveTotalPage(calculateTotalPages);
+
+    //Scroll up
+
+    const jumbotron = document.querySelector('.jumbotron');
+    jumbotron.scrollIntoView({behavior:'smooth'})
+
+    }
+
+    ConsultAPI();
+    
+
+  }, [search, page]);
+
+  //Previous page
+
+  const previouspage = () => {
+
+    const newPage = page - 1;
+
+    if(newPage === 0) return;
+
+    savePage(newPage);
+
+  }
+
+  //Next page
+
+
+  const nextpage = () => {
+
+    const newPage = page + 1;
+
+    if(newPage > totalpage) return;
+ 
+    savePage(newPage);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <div className="jumbotron">
+        <p className="lead text-center">Pic Catcher</p>
+        <Form
+          saveSearch={saveSearch}/>
+      </div>
+      <div className="row justify-content-center">
+        {error ? <Error message="No Results"/> : null}
+        <ImageList
+        images={images}
+        />
+
+        {(page === 1 || error)
+          ? null
+          :(
+            <button
+            type="button"
+            className="btn btn-info mr-1 p-2"
+            onClick={previouspage}
+            > &laquo; Previous</button>
+        )}
+        
+        {(page === totalpage || search === '' || error)
+          ? null
+          :(
+            <button
+            type="button"
+            className="btn btn-info p-2"
+            onClick={nextpage}
+            >Next &raquo;</button>
+        )}
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import Error from './Error'
+import PropTypes from 'prop-types';
+
+
+const Form = ({saveSearch}) => {
+
+    const [ term, saveTerm] = useState('');
+    const [error, saveError] = useState(false);
+
+    const Search = e =>{
+
+    e.preventDefault();
+
+    //Validate
+
+    if(term.trim() === ''){
+
+        saveError(true);
+        return;
+    }
+
+    saveError(false);
+
+    saveSearch(term);
+
+    }
+
+
+
+    return ( 
+        <form
+        onSubmit={Search}>
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input
+                    type="text"
+                    className="form-control form-control-md"
+                    placeholder="Search image"
+                    onChange= {e => saveTerm(e.target.value)}
+                    />
+                </div>
+                <div className="form-group col-md-4">
+                    <input
+                    type="submit"
+                    className="btn btn-md btn-secondary  btn-block"
+                    value="Search"
+                    />
+                </div>
+
+            </div>
+            {error ? <Error message="Please. Add a term" /> :null}
+        </form>
+     );
+}
+
+Form.propTypes = {
+    saveSearch:PropTypes.func.isRequired
+}
+ 
+export default Form;
